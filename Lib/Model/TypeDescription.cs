02e854ecdee098e2924f6b9d9eee27981132ca4f
Michał Zegan <webczat_200@poczta.onet.pl>
2020-02-11 19:01:47 +0100

    Add properties and tests for most type modifiers
    Supports abstract, static, ref, readonly, sealed, and notion of generic types.

--- Lib/Model/TypeDescription.cs
+++ Lib/Model/TypeDescription.cs
@@ -2,6 +2,7 @@
 // All rights reserved.
 // This file is licensed under the BSD-2-Clause license, see 'LICENSE' file in source root for more details.
 
+using System.Diagnostics;
 using Microsoft.CodeAnalysis;
 
 namespace Apiview.Model
@@ -18,12 +19,53 @@ namespace Apiview.Model
         /// Initializes a new instance of the <see cref="TypeDescription"/> class.
         /// </summary>
         /// <param name="symbol">The wrapped roslyn symbol.</param>
-        public TypeDescription(INamedTypeSymbol symbol) => this.symbol = symbol;
+        public TypeDescription(INamedTypeSymbol symbol)
+        {
+            Debug.Assert(!(symbol is IErrorTypeSymbol), $"{nameof(TypeDescription)} does not support unresolved types");
+            Debug.Assert(!symbol.IsImplicitlyDeclared, $"{nameof(TypeDescription)} does not support compiler generated types");
+            this.symbol = symbol;
+        }
 
         /// <summary>
         /// Gets a type name without namespace.
         /// </summary>
         /// <value>The type name without namespace.</value>
         public string Name => this.symbol.MetadataName;
+
+        /// <summary>
+        /// Gets a value indicating whether the type is abstract.
+        /// </summary>
+        /// <value><c>true</c> when the type is abstract.</value>
+        public bool IsAbstract => this.symbol.IsAbstract;
+
+        /// <summary>
+        /// Gets a value indicating whether the type is sealed.
+        /// </summary>
+        /// <value><c>true</c> if the type is sealed.</value>
+        public bool IsSealed => this.symbol.IsSealed;
+
+        /// <summary>
+        /// Gets a value indicating whether the type is generic.
+        /// </summary>
+        /// <value><c>true</c> when the type is generic.</value>
+        public bool IsGeneric => this.symbol.IsGenericType;
+
+        /// <summary>
+        /// Gets a value indicating whether the type is static.
+        /// </summary>
+        /// <value><c>true</c> if the type is static.</value>
+        public bool IsStatic => this.symbol.IsStatic;
+
+        /// <summary>
+        /// Gets a value indicating whether the type is ref-like (ref struct).
+        /// </summary>
+        /// <value><c>true</c> if the type is ref-like.</value>
+        public bool IsRefLike => this.symbol.IsRefLikeType;
+
+        /// <summary>
+        /// Gets a value indicating whether the type is readonly.
+        /// </summary>
+        /// <value><c>true</c> if the type is readonly.</value>
+        public bool IsReadonly => this.symbol.IsReadOnly;
     }
 }
--- LibTests/Model/TypeDescriptionTests.cs
+++ LibTests/Model/TypeDescriptionTests.cs
@@ -47,5 +47,185 @@ namespace Apiview.Tests.Model
 
             Assert.Equal("TestType`2", name);
         }
+
+        [Fact]
+        public void IsAbstractReturnsTrueWhenTypeAbstract()
+        {
+            var source = @"
+            abstract class TestClass
+            {
+            }
+            ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestClass");
+
+            var value = new TypeDescription(symbol).IsAbstract;
+
+            Assert.True(value);
+        }
+
+        [Fact]
+        public void IsAbstractReturnsFalseWhenTypeNotAbstract()
+        {
+            var source = @"
+            class TestClass
+            {
+            }
+            ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestClass");
+
+            var value = new TypeDescription(symbol).IsAbstract;
+
+            Assert.False(value);
+        }
+
+        [Fact]
+        public void IsSealedReturnsTrueWhenTypeSealed()
+        {
+            var source = @"
+                    sealed class TestClass
+                    {
+                    }
+                    ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestClass");
+
+            var value = new TypeDescription(symbol).IsSealed;
+
+            Assert.True(value);
+        }
+
+        [Fact]
+        public void IsSealedReturnsFalseWhenTypeNotSealed()
+        {
+            var source = @"
+                    class TestClass
+                    {
+                    }
+                    ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestClass");
+
+            var value = new TypeDescription(symbol).IsSealed;
+
+            Assert.False(value);
+        }
+
+        [Fact]
+        public void IsGenericReturnsTrueWhenTypeGeneric()
+        {
+            var source = @"
+                            class TestClass<T1, T2>
+                            {
+                            }
+                            ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestClass`2");
+
+            var value = new TypeDescription(symbol).IsGeneric;
+
+            Assert.True(value);
+        }
+
+        [Fact]
+        public void IsGenericReturnsFalseWhenTypeNotGeneric()
+        {
+            var source = @"
+                            class TestClass
+                            {
+                            }
+                            ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestClass");
+
+            var value = new TypeDescription(symbol).IsGeneric;
+
+            Assert.False(value);
+        }
+
+        [Fact]
+        public void IsStaticReturnsTrueWhenTypeIsStatic()
+        {
+            var source = @"
+                                    static class TestClass
+                                    {
+                                    }
+                                    ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestClass");
+
+            var value = new TypeDescription(symbol).IsStatic;
+
+            Assert.True(value);
+        }
+
+        [Fact]
+        public void IsStaticReturnsFalseWhenTypeIsNotStatic()
+        {
+            var source = @"
+                                    class TestClass
+                                    {
+                                    }
+                                    ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestClass");
+
+            var value = new TypeDescription(symbol).IsStatic;
+
+            Assert.False(value);
+        }
+
+        [Fact]
+        public void IsRefLikeReturnsTrueWhenTypeIsRefLike()
+        {
+            var source = @"
+            ref struct TestStruct
+            {
+            }
+            ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestStruct");
+
+            var value = new TypeDescription(symbol).IsRefLike;
+
+            Assert.True(value);
+        }
+
+        [Fact]
+        public void IsRefLikeReturnsFalseWhenTypeNotRefLike()
+        {
+            var source = @"
+            struct TestStruct
+            {
+            }
+            ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestStruct");
+
+            var value = new TypeDescription(symbol).IsRefLike;
+
+            Assert.False(value);
+        }
+
+        [Fact]
+        public void IsReadonlyReturnsTrueIfTypeIsReadonly()
+        {
+            var source = @"
+            readonly struct TestStruct
+            {
+            }
+            ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestStruct");
+
+            var value = new TypeDescription(symbol).IsReadonly;
+
+            Assert.True(value);
+        }
+
+        [Fact]
+        public void IsReadonlyReturnsFalseWhenTypeNotReadonly()
+        {
+            var source = @"
+            struct TestStruct
+            {
+            }
+            ";
+            var symbol = CreateCompilation(source).GetTypeByMetadataName("TestStruct");
+
+            var value = new TypeDescription(symbol).IsReadonly;
+
+            Assert.False(value);
+        }
     }
 }
